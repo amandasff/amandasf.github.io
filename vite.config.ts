1b2dfe8d0453c165de2bfc31884f9221b33a03ae
amandasff <amanda.sf.wu@gmail.com>
2025-04-01 00:21:27 -0400

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
